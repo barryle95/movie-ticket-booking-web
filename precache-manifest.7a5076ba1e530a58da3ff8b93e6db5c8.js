a4b9c04af340623859134289835910617198b605
barryle95 <seagull13vn@gmail.com>
2019-10-22 14:14:40 +0700

    Updates

---
+++ precache-manifest.7a5076ba1e530a58da3ff8b93e6db5c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f04ab8064dcd6f136762a09aa1c993ae",
+    "url": "/barryle95/movie-ticket-booking-web/index.html"
+  },
+  {
+    "revision": "1c8c9eac9214211076c6",
+    "url": "/barryle95/movie-ticket-booking-web/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "cd8eb976e37cdbd6815f",
+    "url": "/barryle95/movie-ticket-booking-web/static/js/2.391021ad.chunk.js"
+  },
+  {
+    "revision": "1c8c9eac9214211076c6",
+    "url": "/barryle95/movie-ticket-booking-web/static/js/main.3cc7b75a.chunk.js"
+  },
+  {
+    "revision": "059ac12ac76472924435",
+    "url": "/barryle95/movie-ticket-booking-web/static/js/runtime-main.8148c1af.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/barryle95/movie-ticket-booking-web/static/media/logo.25bf045c.svg"
+  }
+]);

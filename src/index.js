a995f2963b8805d3a69799bb244bcb86088b6967
barryle95 <seagull13vn@gmail.com>
2019-11-06 10:49:08 +0700

    add navbar, local bootstrap, jquery, popper.js

--- src/index.js
+++ src/index.js
@@ -9,6 +9,13 @@ import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import rootReducer from './redux/reducers/rootReducer';
 
+//bootstrap, jquery, popper.js
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/js/bootstrap.min.js";
+import 'popper.js/dist/umd/popper.min.js';
+import 'jquery/dist/jquery.min.js';
+
+
 const store = createStore(
     rootReducer,
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()

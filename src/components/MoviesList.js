8644bfad61452742aa33bb48ba31ba5440643bc3
barryle95 <seagull13vn@gmail.com>
2019-11-15 15:00:14 +0700

    edit Admin.js, index.js, purpose: add loginAdmin function

---
+++ src/components/MoviesList.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as action from './../redux/action/index';
+import { NavLink } from 'react-router-dom';
+
+class MoviesList extends Component {
+
+    renderHTML = () => {
+        const { moviesList } = this.props;
+        console.log(moviesList);
+        return moviesList.map((item, index) => {
+            console.log(item)
+            return (
+                <div key={index}>
+                    <div className="col-xs-12 col-sm-6 col-md-6 col-lg-3 col-lg-3">
+                        <div className="container">
+                            <div className="card bg-light" style={{ width: 300 }}>
+                                <img className="card-img-top" src={item.hinhAnh} alt="Card" style={{ maxWidth: '100%', height: 250 }} />
+                                <div className="card-body text-center">
+                                    <h4 className="card-title text-center">{item.tenPhim}</h4>
+                                    <div>
+                                        <p className="card-text">{item.moTa}</p>
+                                    </div>
+
+                                    <NavLink to={`detail-movie/${item.maPhim}`} className="btn btn-primary mr-1">Detail</NavLink>
+                                    <a href="#a" className="btn btn-danger" onClick={() => this.toggleButton()}>Toggle</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            )
+        })
+    };
+
+    render() {
+        return (
+            <div className="row">
+                {this.renderHTML()}
+            </div>
+        )
+    }
+}
+
+const mapDispatchToProps = dispatch => ({
+    saveMoviesList: () => {
+        dispatch(action.saveMoviesAPI())
+    }
+});
+
+const mapStateToProps = state => ({
+    moviesList: state.moviesReducer.moviesList,
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(MoviesList);
--- src/pages/admin/Admin.js
+++ src/pages/admin/Admin.js
@@ -1,11 +1,53 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import {connect} from 'react-redux';
+import * as action from '../../redux/action/index';
 
-export default class Admin extends Component {
+class Admin extends Component {
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+             taiKhoan: "",
+             matKhau: "",
+        }
+    }
+
+    handleOnChange = (e) => {
+        let { name, value } = e.target;
+        this.setState({
+            [name]: value,
+        })
+    }
+
+    handleOnSubmit = (e) => {
+        e.preventDefault();
+        console.log(this.props);
+        this.props.login(this.state, this.props.history)
+    }
+    
     render() {
         return (
-            <div>
-                Admin
-            </div>
+            <form className="container" onSubmit={this.handleOnSubmit}>
+                <div className="form-group">
+                    <label htmlFor>Username</label>
+                    <input type="text" className="form-control" name="taiKhoan" id aria-describedby="helpId" placeholder onChange={this.handleOnChange}/>
+                </div>
+                <div className="form-group">
+                    <label htmlFor>Password</label>
+                    <input type="password" className="form-control" name="matKhau" id aria-describedby="helpId" placeholder onChange={this.handleOnChange}/>
+                </div>
+                <div className="form-group">
+                    <button name="" id="" class="btn btn-primary" type="submit" value="">Login</button>
+                </div>
+            </form>
         )
     }
 }
+
+const mapDispatchToProps = dispatch => ({
+    login: (user, history) => {
+        dispatch(action.actLoginAdmin(user, history))
+    }
+});
+
+export default connect(null, mapDispatchToProps)(Admin);
